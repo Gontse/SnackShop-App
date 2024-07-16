d2b2c3a36998fabeb6d249077f4999e474c08b37
Gontse Ranoto (Gontze) <gontseandries@gmail.com>
2024-07-16 14:46:04 +0200

    Initial Commit

---
+++ SnackShop/SnackShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnackShopApp.swift
+//  SnackShop
+//
+//  Created by Gontse on 2024/07/16.
+//
+
+import SwiftUI
+
+@main
+struct SnackShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SnackShopTests/SnackShopTests.swift
@@ -0,0 +1,17 @@
+//
+//  SnackShopTests.swift
+//  SnackShopTests
+//
+//  Created by Gontse on 2024/07/16.
+//
+
+import Testing
+@testable import SnackShop
+
+struct SnackShopTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
