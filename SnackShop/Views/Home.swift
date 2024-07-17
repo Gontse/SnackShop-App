e9918c58eb76a0a5ea8d15f91c7fa18b4454f490
Gontse Ranoto (Gontze) <gontseandries@gmail.com>
2024-07-17 03:42:07 +0200

    Update Home.swift

--- SnackShop/Views/Home.swift
+++ SnackShop/Views/Home.swift
@@ -97,20 +97,49 @@ struct ProductCard: View {
   
   var body: some View {
     ZStack {
+      Image(product.image)
       VStack(alignment: .leading) {
         Text("\(product.name)")
           .font(.system(size: 30, weight: .semibold))
+          .frame(width: 140)
         
         Text(product.category.rawValue)
           .font(.callout)
           .padding()
           .background(.white.opacity(0.5))
           .clipShape(Capsule())
+        
+        Spacer()
+        
+        HStack {
+          Text("R\(product.price)")
+            .font(.system(size: 25, weight: .semibold))
+    
+          Spacer()
+          
+          Button {
+            
+          } label: {
+            Image(systemName: "basket")
+              .foregroundColor(.white)
+              .imageScale(.large)
+              .padding()
+              .frame(width: 90, height: 60)
+              .background(.black)
+              .clipShape(Capsule())
+          }
+        }
+        .padding(.leading)
+        .padding()
+        .frame(maxWidth: .infinity)
+        .frame(height: 80)
+        .background(.white.opacity(0.5))
+        .clipShape(Capsule())
       }
     }
-    .padding()
+    .padding(30)
     .frame(width: 336, height: 422)
-    .background(product.color.opacity(0.2))
+    .background(product.color.opacity(0.15))
     .clipShape(.rect(cornerRadius: 57))
     .padding(.leading, 20)
   }
